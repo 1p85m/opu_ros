f936b6fe0ec0c489cec4e86127fc26531b2eed7a
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-01-22 20:33:03 +0900

    bug fix

--- lib/calc_coord.py
+++ lib/calc_coord.py
@@ -51,7 +51,7 @@ class azel_calc(object):
         target_el = el+ret[1]
         return target_az, target_el
 
-    def coordinate_calc(self, x_list, y_list, time_list, coord, off_az, off_el, hosei, lamda,　limit=True, rotation=True):
+    def coordinate_calc(self, x_list, y_list, time_list, coord, off_az, off_el, hosei, lamda, limit=True, rotation=True):
         """
         x_list and y_list == [deg]
         """
